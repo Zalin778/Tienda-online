6d06387315be83f4b55e452fb1f7e6ca0f34901e
Zalin778 <126732700+Zalin778@users.noreply.github.com>
2023-03-01 20:47:19 -0500

    Creando main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Obtener el formulario
+var formulario = document.querySelector("form");
+
+// Agregar un listener para el evento submit del formulario
+formulario.addEventListener("submit", function(evento) {
+  evento.preventDefault(); // Prevenir que el formulario se envíe
+
+  // Obtener los valores de los campos de nombre, correo y contraseña
+  var nombre = formulario.elements.nombre.value;
+  var correo = formulario.elements.correo.value;
+  var contrasena = formulario.elements.contrasena.value;
+
+  // Validar que los campos no estén vacíos
+  if (nombre === "" || correo === "" || contrasena === "") {
+    alert("Por favor, complete todos los campos.");
+    return;
+  }
+
+  // Validar que la contraseña tenga al menos 8 caracteres
+  if (contrasena.length < 8) {
+    alert("La contraseña debe tener al menos 8 caracteres.");
+    return;
+}
